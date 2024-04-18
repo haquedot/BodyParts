fcd4da7ee994b37b5cffbab317582d993a50b82a
Meraj <haquedot@gmail.com>
2024-04-18 22:28:13 +0530

    start work on main area of the game'

---
+++ script.js
@@ -0,0 +1,24 @@
+const bodyBg = document.getElementsByTagName('body')[0];
+const boyBtn = document.getElementById('boyBtn');
+const girlBtn = document.getElementById('girlBtn');
+const genderDiv = document.getElementById('genderDiv');
+const nextBtn = document.getElementById('nextBtn');
+const gameArea = document.getElementById('gameArea');
+
+function boy() {
+    genderDiv.classList.add('d-none');
+    bodyBg.style.backgroundImage = "url('../images/conversation-boy-bg.svg')";
+    nextBtn.classList.remove('d-none');
+}
+
+function girl() {
+    genderDiv.classList.add('d-none');
+    bodyBg.style.backgroundImage = "url('../images/conversation-boy-bg.svg')";
+    nextBtn.classList.remove('d-none');
+}
+
+function next(){
+    bodyBg.style.backgroundImage = "url('../images/classroom.svg')";
+    nextBtn.classList.add('d-none');
+    gameArea.classList.remove('d-none')
+}
